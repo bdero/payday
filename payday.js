b976e73cf65714986fb1248c2ab446686e2e46ec
Brandon DeRosier <bdero@mit.edu>
2015-07-22 20:29:15 -0400

    Bill listing, bill creation, and validation

--- payday.js
+++ payday.js
@@ -1,23 +1,49 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
+var Bills = new Mongo.Collection("bills");
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.publish("bills", function() {
+    return Bills.find({});
   });
 }
+
+if (Meteor.isClient) {
+  Meteor.subscribe("bills");
+
+  Template.bills.helpers({
+    bills: function () {
+      return Bills.find({});
+    }
+  });
+}
+
+Meteor.methods({
+  createBill: function(bill) {
+    // Make sure the user is authenticated
+    var user = Meteor.user();
+    if (Meteor.user() === null) {
+      throw new Meteor.Error(
+        "not-authorized",
+        "User not authorized to perform this action"
+      );
+    }
+
+    // Validate types
+    check(bill, {
+      name: String,
+      price: Number,
+      period: String,
+      due: Number,
+      owner: String
+    });
+
+    // Validate that the selected user exists
+    if (Meteor.users.findOne({_id: bill.owner})) {
+      throw new Meteor.Error(
+        "invalid-user",
+        "Couldn't find user with ID: " + bill.owner
+      );
+    }
+
+    Bills.insert(bill);
+  }
+});
