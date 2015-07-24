44d02ba7e592040c18112a807e41c2bdee11c088
Brandon DeRosier <bdero@mit.edu>
2015-07-23 21:43:58 -0400

    Add jQuery and Lodash; add bill form elements

--- app/client/client.js
+++ app/client/client.js
@@ -17,6 +17,45 @@ var getUserWithDisplay = function(user) {
   return user;
 };
 
+var daySelection = _.range(1, 32);
+var dueWordMap = {
+  "weekly": {
+    selectName: "Weekday",
+    prefix: "on",
+    selection: _.zip(
+      _.range(7),
+      [
+        "Sunday", "Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday"
+      ]
+    )
+  },
+  "monthly": {
+    selectName: "Day",
+    prefix: "on day",
+    selection: _.zip(
+      daySelection,
+      _.map(daySelection, function(day) {
+        return day.toString();
+      })
+    )
+  },
+  "yearly": {
+    selectName: "Month",
+    prefix: "in the month of",
+    selection: _.zip(
+      _.range(12),
+      [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+      ]
+    )
+  },
+};
+var dueMap = function() {
+  return dueWordMap[Session.get("periodSelected")];
+};
+
 Template.bills.helpers({
   bills: function () {
     return Bills.find({}).map(function(bill) {
@@ -41,5 +80,22 @@ Template.createBillForm.helpers({
   },
   currentUserIdIs: function(userId) {
     return Meteor.user()._id === this._id;
+  },
+  periodSelected: function() {
+    return Session.get("periodSelected")
+  },
+  dueMap: dueMap
+});
+Template.createBillForm.events({
+  "change #createBill #period": function(event) {
+    Session.set("periodSelected", $("#createBill #period")[0].value);
+
+    // Set due selection
+    $("#createBill #due option:selected").attr("selected", null);
+    $("#createBill #due option[value=\"\"]").attr("selected", "selected");
   }
+});
+
+Template.dueSelect.helpers({
+  dueMap: dueMap
 });
